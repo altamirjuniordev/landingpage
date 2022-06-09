3bfb52fd4d3e58223cb4749eb618cc18e109ae0c
Altamir Junior <105239024+altamirjuniordev@users.noreply.github.com>
2022-06-08 22:37:10 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,57 @@
+// Typescript JS
+var typed = new Typed(".typing",{
+    strings: ["Web Design","Web Developer", "Criação de sites"],
+    typeSpeed: 80,
+    backSpeed: 80,
+});
+
+var typed = new Typed(".typing-2",{
+    strings: ["Desenvolvimento Web","Resolução de bugs em sites", "Criação e manutenção FrontEnd"],
+    typeSpeed: 100,
+    backSpeed: 80,
+});
+
+// Show/Hide FAQs answer
+const faqs = document.querySelectorAll('.faq')
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon
+        const icon = faq.querySelector('.faq_icone i');
+        if (icon.className === 'fa-solid fa-plus'){
+            icon.className = 'fa-solid fa-minus';
+        }
+        else {
+            icon.className = 'fa-solid fa-plus';
+        }
+    })
+})
+
+//Nav menu
+
+const menu = document.querySelector('.nav_menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display="inline-block";
+    menuBtn.style.display = "none";
+
+})
+
+//fechar nav
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block"
+}
+closeBtn.addEventListener('click', closeNav);
+
+//nav color change on scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY>100);
+})
+
+
